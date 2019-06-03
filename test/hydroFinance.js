c695b7a733e6c51263c60ef966b88fa51eb42770
merlox <merunasgrincalaitis@gmail.com>
2019-06-03 20:55:35 +0200

    Workin on the tests

--- test/hydroFinance.js
+++ test/hydroFinance.js
@@ -17,19 +17,48 @@ contract('HydroFinance', accounts => {
         console.log('Creating identity two')
         await identityRegistry.createIdentity(accounts[1], [accounts[2]], [accounts[2]], { from: accounts[1], gas: 8e6 })
     })
-    it('should add a new card and create a new user', async () => {
+    it('should add a new card', async () => {
         const card = '1234123412341234'
         const expiry = Math.floor(Date.now() / 1000)
         const name = 'My credit card'
         const cvv = '123'
 
-        // await hydroFinance.addCard(card, expiry, name, cvv, {
-        //     from: accounts[0],
-        //     gas: 7e6
-        // })
+        await hydroFinance.addCard(card, expiry, name, cvv, {
+            from: accounts[0],
+            gas: 7e6
+        })
+
+        const publishedCard = await hydroFinance.cardById(1)
+        assert.equal(parseInt(publishedCard.id), 1, 'The published card id must be one')
+        assert.equal(publishedCard.einOwner, 1, 'The published card ein owner must be one')
+        assert.equal(publishedCard.cardName, name, 'The published card name must be <My credit card>')
+        assert.equal(String(publishedCard.card), card, 'The published card id must be one')
+        assert.equal(Number(publishedCard.expiry), expiry, 'The published card expiry must be the same')
+        assert.equal(String(publishedCard.cvv), cvv, 'The published card cvv must be 123')
+    })
+    it('should create a new user when adding a card', async () => {
+        const card = '1234123412341234'
+        const expiry = Math.floor(Date.now() / 1000)
+        const name = 'My credit card'
+        const cvv = '123'
+
+        await hydroFinance.addCard(card, expiry, name, cvv, {
+            from: accounts[0],
+            gas: 7e6
+        })
+
+        const publishedUser = await hydroFinance.getUserData()
+        assert.equal(parseInt(publishedUser[0]), 1, 'The published user ein must be set')
+        assert.equal(publishedUser[1], accounts[0], 'The published user address must be set')
+        assert.equal(publishedUser[1], accounts[0], 'The published user address must be set')
+
+        console.log(parseInt(publishedUser[0]))
+
         //
-        // const publishedCard = await hydroFinance.cardById(1)
-        // console.log('Card', publishedCard)
-        // assert.equal(title, product.title, 'The product must be deployed with the publish function')
+        // uint256 einOwner;
+        // address owner;
+        // uint256[] cardIds;
+        // uint256[] bankIds;
+        // uint256[] investmentIds;
     })
 }) // All tests passing
